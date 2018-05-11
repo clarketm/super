2c7cae8b726a1d7b8b0679b6b3f8b8a6bfe8fbb0
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-11 13:31:49 -0700

    add transform-builtin-extend and minify to babel preset

--- packages/babel-preset-super/index.js
+++ packages/babel-preset-super/index.js
@@ -1,9 +1,17 @@
 module.exports = {
-
   presets: [
-    require('babel-preset-env'),
-    require('babel-preset-stage-0'),
-    require('babel-preset-flow'),
-  ]
+    require("babel-preset-env"),
+    require("babel-preset-stage-0"),
+    require("babel-preset-flow"),
+    require("babel-preset-minify")
+  ],
 
+  plugins: [
+    [
+      require("babel-plugin-transform-builtin-extend").default,
+      {
+        globals: ["Set"]
+      }
+    ]
+  ]
 };
