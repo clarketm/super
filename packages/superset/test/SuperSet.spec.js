4695f1fca0bb636fe4a126e34ee946d0eac6fc6b
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-12 17:22:44 -0700

    add isSuperset and isSubset Set methods

--- packages/superset/test/SuperSet.spec.js
+++ packages/superset/test/SuperSet.spec.js
@@ -1,47 +1,104 @@
 const { assert } = require("chai");
-const { SuperSet } = require("../lib/SuperSet");
+const { SuperSet } = require("../src/lib/SuperSet");
 
 describe("SuperSet", () => {
   let setA, setB, expected, actual;
 
-  beforeEach(() => {
-    setA = new SuperSet([1, 2, 3]);
-    setB = new SuperSet([2, 3, 4]);
-  });
+  describe("membership", () => {
+    describe("#isSubset", () => {
+      it("should be a subset of setB", () => {
+        setA = new SuperSet([1, 2]);
+        setB = new SuperSet([1, 2, 3]);
 
-  describe("#union", () => {
-    it("should perform a set union between two sets", () => {
-      expected = new SuperSet([1, 2, 3, 4]);
-      actual = setA.union(setB);
+        expected = true;
+        actual = setA.isSubset(setB);
 
-      assert.deepEqual(actual, expected);
+        assert.equal(actual, expected);
+      });
+
+      it("should not be a subset of setB", () => {
+        setA = new SuperSet([4, 5]);
+        setB = new SuperSet([1, 2, 3]);
+
+        expected = false;
+        actual = setA.isSubset(setB);
+
+        assert.equal(actual, expected);
+      });
+    });
+
+    describe("#isSuperset", () => {
+      it("should be a subset of setB", () => {
+        setA = new SuperSet([1, 2, 3]);
+        setB = new SuperSet([1, 2]);
+
+        expected = true;
+        actual = setA.isSuperset(setB);
+
+        assert.equal(actual, expected);
+      });
+
+      it("should not be a subset of setB", () => {
+        setA = new SuperSet([1, 2, 3]);
+        setB = new SuperSet([4, 5]);
+
+        expected = false;
+        actual = setA.isSuperset(setB);
+
+        assert.equal(actual, expected);
+      });
     });
   });
 
-  describe("#intersection", () => {
-    it("should perform a set `intersection` between two sets", () => {
-      expected = new SuperSet([2, 3]);
-      actual = setA.intersection(setB);
-
-      assert.deepEqual(actual, expected);
+  describe("operations", () => {
+    beforeEach(() => {
+      setA = new SuperSet([1, 2, 3]);
+      setB = new SuperSet([2, 3, 4]);
     });
-  });
 
-  describe("#difference", () => {
-    it("should perform a set `difference` between two sets", () => {
-      expected = new SuperSet([1]);
-      actual = setA.difference(setB);
+    describe("#union", () => {
+      it("should check if is setA is a superset of setB", () => {
+        expected = new SuperSet([1, 2, 3, 4]);
+        actual = setA.union(setB);
 
-      assert.deepEqual(actual, expected);
+        assert.deepEqual(actual, expected);
+      });
     });
-  });
 
-  describe("#symmetricDifference", () => {
-    it("should perform a set `symmetricDifference` between two sets", () => {
-      expected = new SuperSet([1, 4]);
-      actual = setA.symmetricDifference(setB);
+    describe("#union", () => {
+      it("should perform a set union between two sets", () => {
+        expected = new SuperSet([1, 2, 3, 4]);
+        actual = setA.union(setB);
 
-      assert.deepEqual(actual, expected);
+        assert.deepEqual(actual, expected);
+      });
+    });
+
+    describe("#intersection", () => {
+      it("should perform a set `intersection` between two sets", () => {
+        expected = new SuperSet([2, 3]);
+        actual = setA.intersection(setB);
+
+        assert.deepEqual(actual, expected);
+      });
+    });
+
+    describe("#difference", () => {
+      it("should perform a set `difference` between two sets", () => {
+        expected = new SuperSet([1]);
+        actual = setA.difference(setB);
+
+        assert.deepEqual(actual, expected);
+      });
+    });
+
+    describe("#symmetricDifference", () => {
+      it("should perform a set `symmetricDifference` between two sets", () => {
+        expected = new SuperSet([1, 4]);
+        actual = setA.symmetricDifference(setB);
+
+        assert.deepEqual(actual, expected);
+      });
     });
   });
 });
