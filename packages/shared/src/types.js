18f79caf84050447a0aa40f1c770158deeb2e170
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-13 04:59:09 -0700

    move Item to shared -> types.js file

---
+++ packages/shared/src/types.js
@@ -0,0 +1,15 @@
+/**
+ * @typedef {null|undefined|boolean|number|string|Symbol|Function|Array|Date|Object} Item
+ */
+
+export type Item =
+  | null
+  | undefined
+  | boolean
+  | number
+  | string
+  | Symbol
+  | Function
+  | Array
+  | Date
+  | Object;
