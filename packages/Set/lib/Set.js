8ad45a109ce5580577a731fdd67da04f0d69fbc5
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-14 11:18:08 -0700

    prepare Set

--- packages/Set/lib/Set.js
+++ packages/Set/lib/Set.js
@@ -11,11 +11,44 @@
   (factory((global.Set = {})));
 }(this, (function (exports) { 'use strict';
 
-  // 
+  var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+  var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+  function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+  function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+  function _extendableBuiltin(cls) {
+    function ExtendableBuiltin() {
+      var instance = Reflect.construct(cls, Array.from(arguments));
+      Object.setPrototypeOf(instance, Object.getPrototypeOf(this));
+      return instance;
+    }
+
+    ExtendableBuiltin.prototype = Object.create(cls.prototype, {
+      constructor: {
+        value: cls,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+
+    if (Object.setPrototypeOf) {
+      Object.setPrototypeOf(ExtendableBuiltin, cls);
+    } else {
+      ExtendableBuiltin.__proto__ = cls;
+    }
+
+    return ExtendableBuiltin;
+  }
 
   /**
+   * 
    * @module super/set
-   *
    */
 
   /**
@@ -29,14 +62,21 @@
    * @public
    *
    */
-  var _Set = (function (Set) {
-    function _Set(iterable) {
-      Set.call(this, iterable);
-    }
+  var _Set = function (_extendableBuiltin2) {
+    _inherits(_Set, _extendableBuiltin2);
 
-    if ( Set ) _Set.__proto__ = Set;
-    _Set.prototype = Object.create( Set && Set.prototype );
-    _Set.prototype.constructor = _Set;
+    /**
+     * @public
+     *
+     * @desc Construct a Set
+     *
+     * @param {Iterable} iterable
+     */
+    function _Set(iterable) {
+      _classCallCheck(this, _Set);
+
+      return _possibleConstructorReturn(this, (_Set.__proto__ || Object.getPrototypeOf(_Set)).call(this, iterable));
+    }
 
     /**
      * @public
@@ -46,175 +86,355 @@
      * @param {Callback} callback - callback function
      * @returns {boolean} true if the callback function returns a truthy value for any set element; otherwise, false
      */
-    _Set.prototype.some = function some (callback) {
-      var this$1 = this;
 
-      var result;
 
-      for (var i = 0, list = this$1.entries(); i < list.length; i += 1) {
-        var ref = list[i];
-        var value1 = ref[0];
-        var value2 = ref[1];
+    _createClass(_Set, [{
+      key: "some",
+      value: function some(callback) {
+        var result = void 0;
 
-        result = callback(value1, value2, this$1);
-        if (result) { return true; }
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+          for (var _iterator = this.entries()[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var _ref = _step.value;
+
+            var _ref2 = _slicedToArray(_ref, 2);
+
+            var value1 = _ref2[0];
+            var value2 = _ref2[1];
+
+            result = callback(value1, value2, this);
+            if (result) return true;
+          }
+        } catch (err) {
+          _didIteratorError = true;
+          _iteratorError = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+              _iterator.return();
+            }
+          } finally {
+            if (_didIteratorError) {
+              throw _iteratorError;
+            }
+          }
+        }
+
+        return false;
       }
 
-      return false;
-    };
+      /**
+       * @public
+       *
+       * @desc Test whether all elements in the set pass the test implemented by the provided function
+       *
+       * @param {Callback} callback - callback function
+       * @returns {boolean} true if the callback function returns a truthy value for every set element; otherwise, false
+       */
 
-    /**
-     * @public
-     *
-     * @desc Test whether all elements in the set pass the test implemented by the provided function
-     *
-     * @param {Callback} callback - callback function
-     * @returns {boolean} true if the callback function returns a truthy value for every set element; otherwise, false
-     */
-    _Set.prototype.every = function every (callback) {
-      var this$1 = this;
+    }, {
+      key: "every",
+      value: function every(callback) {
+        var result = void 0;
 
-      var result;
+        var _iteratorNormalCompletion2 = true;
+        var _didIteratorError2 = false;
+        var _iteratorError2 = undefined;
 
-      for (var i = 0, list = this$1.entries(); i < list.length; i += 1) {
-        var ref = list[i];
-        var value1 = ref[0];
-        var value2 = ref[1];
+        try {
+          for (var _iterator2 = this.entries()[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+            var _ref3 = _step2.value;
 
-        result = callback(value1, value2, this$1);
-        if (!result) { return false; }
+            var _ref4 = _slicedToArray(_ref3, 2);
+
+            var value1 = _ref4[0];
+            var value2 = _ref4[1];
+
+            result = callback(value1, value2, this);
+            if (!result) return false;
+          }
+        } catch (err) {
+          _didIteratorError2 = true;
+          _iteratorError2 = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion2 && _iterator2.return) {
+              _iterator2.return();
+            }
+          } finally {
+            if (_didIteratorError2) {
+              throw _iteratorError2;
+            }
+          }
+        }
+
+        return true;
       }
 
-      return true;
-    };
+      /**
+       * @public
+       *
+       * @desc Subset of a set
+       *
+       * @param {Set} setB - SetB
+       * @returns {boolean} setA is subset of setB
+       */
 
-    /**
-     * @public
-     *
-     * @desc Subset of a set
-     *
-     * @param {Set} setB - SetB
-     * @returns {boolean} setA is subset of setB
-     */
-    _Set.prototype.isSubset = function isSubset (setB) {
-      var setA = this;
+    }, {
+      key: "isSubset",
+      value: function isSubset(setB) {
+        var setA = this;
 
-      for (var i = 0, list = setA; i < list.length; i += 1) {
-        var v = list[i];
+        var _iteratorNormalCompletion3 = true;
+        var _didIteratorError3 = false;
+        var _iteratorError3 = undefined;
 
-        if (!setB.has(v)) { return false; }
-      }
-      return true;
-    };
+        try {
+          for (var _iterator3 = setA[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+            var v = _step3.value;
 
-    /**
-     * @public
-     *
-     * @desc Superset of a set
-     *
-     * @param {Set} setB - SetB
-     * @returns {boolean} setA is superset of setB
-     */
-    _Set.prototype.isSuperset = function isSuperset (setB) {
-      var setA = this;
+            if (!setB.has(v)) return false;
+          }
+        } catch (err) {
+          _didIteratorError3 = true;
+          _iteratorError3 = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion3 && _iterator3.return) {
+              _iterator3.return();
+            }
+          } finally {
+            if (_didIteratorError3) {
+              throw _iteratorError3;
+            }
+          }
+        }
 
-      for (var i = 0, list = setB; i < list.length; i += 1) {
-        var v = list[i];
-
-        if (!setA.has(v)) { return false; }
-      }
-      return true;
-    };
-
-    /**
-     * @public
-     *
-     * @desc Union of setA and setB
-     *
-     * @param {Set} setB - SetB
-     * @returns {Set} setC - union between setA and setB
-     */
-    _Set.prototype.union = function union (setB) {
-      var setA = this;
-      var setC = new Set(setA);
-
-      for (var i = 0, list = setB; i < list.length; i += 1) {
-        var v = list[i];
-
-        setC.add(v);
+        return true;
       }
 
-      return setC;
-    };
+      /**
+       * @public
+       *
+       * @desc Superset of a set
+       *
+       * @param {Set} setB - SetB
+       * @returns {boolean} setA is superset of setB
+       */
 
-    /**
-     * @public
-     *
-     * @desc Intersection of setA and setB
-     *
-     * @param {Set} setB - SetB
-     * @returns {Set} setC - intersection between setA and setB
-     */
-    _Set.prototype.intersection = function intersection (setB) {
-      var setA = this;
-      var setC = new Set();
+    }, {
+      key: "isSuperset",
+      value: function isSuperset(setB) {
+        var setA = this;
 
-      for (var i = 0, list = setB; i < list.length; i += 1) {
-        var v = list[i];
+        var _iteratorNormalCompletion4 = true;
+        var _didIteratorError4 = false;
+        var _iteratorError4 = undefined;
 
-        if (setA.has(v)) { setC.add(v); }
+        try {
+          for (var _iterator4 = setB[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+            var v = _step4.value;
+
+            if (!setA.has(v)) return false;
+          }
+        } catch (err) {
+          _didIteratorError4 = true;
+          _iteratorError4 = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion4 && _iterator4.return) {
+              _iterator4.return();
+            }
+          } finally {
+            if (_didIteratorError4) {
+              throw _iteratorError4;
+            }
+          }
+        }
+
+        return true;
       }
 
-      return setC;
-    };
+      /**
+       * @public
+       *
+       * @desc Union of setA and setB
+       *
+       * @param {Set} setB - SetB
+       * @returns {Set} setC - union between setA and setB
+       */
 
-    /**
-     * @public
-     *
-     * @desc Difference of setA and setB
-     *
-     * @param {Set} setB - SetB
-     * @returns {Set} setC - difference between setA and setB
-     */
-    _Set.prototype.difference = function difference (setB) {
-      var setA = this;
-      var setC = new Set(setA);
+    }, {
+      key: "union",
+      value: function union(setB) {
+        var setA = this;
+        var setC = new Set(setA);
 
-      for (var i = 0, list = setB; i < list.length; i += 1) {
-        var v = list[i];
+        var _iteratorNormalCompletion5 = true;
+        var _didIteratorError5 = false;
+        var _iteratorError5 = undefined;
 
-        setC.delete(v);
+        try {
+          for (var _iterator5 = setB[Symbol.iterator](), _step5; !(_iteratorNormalCompletion5 = (_step5 = _iterator5.next()).done); _iteratorNormalCompletion5 = true) {
+            var v = _step5.value;
+
+            setC.add(v);
+          }
+        } catch (err) {
+          _didIteratorError5 = true;
+          _iteratorError5 = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion5 && _iterator5.return) {
+              _iterator5.return();
+            }
+          } finally {
+            if (_didIteratorError5) {
+              throw _iteratorError5;
+            }
+          }
+        }
+
+        return setC;
       }
 
-      return setC;
-    };
+      /**
+       * @public
+       *
+       * @desc Intersection of setA and setB
+       *
+       * @param {Set} setB - SetB
+       * @returns {Set} setC - intersection between setA and setB
+       */
 
-    /**
-     * @public
-     *
-     * @desc Symmetric difference of setA and setB
-     *
-     * @param {Set} setB - SetB
-     * @returns {Set} setC - difference difference between setA and setB
-     */
-    _Set.prototype.symmetricDifference = function symmetricDifference (setB) {
-      var setA = this;
-      var setC = new Set(setA);
+    }, {
+      key: "intersection",
+      value: function intersection(setB) {
+        var setA = this;
+        var setC = new Set();
 
-      for (var i = 0, list = setB; i < list.length; i += 1) {
-        var v = list[i];
+        var _iteratorNormalCompletion6 = true;
+        var _didIteratorError6 = false;
+        var _iteratorError6 = undefined;
 
-        if (setA.has(v)) { setC.delete(v); }
-        else { setC.add(v); }
+        try {
+          for (var _iterator6 = setB[Symbol.iterator](), _step6; !(_iteratorNormalCompletion6 = (_step6 = _iterator6.next()).done); _iteratorNormalCompletion6 = true) {
+            var v = _step6.value;
+
+            if (setA.has(v)) setC.add(v);
+          }
+        } catch (err) {
+          _didIteratorError6 = true;
+          _iteratorError6 = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion6 && _iterator6.return) {
+              _iterator6.return();
+            }
+          } finally {
+            if (_didIteratorError6) {
+              throw _iteratorError6;
+            }
+          }
+        }
+
+        return setC;
       }
 
-      return setC;
-    };
+      /**
+       * @public
+       *
+       * @desc Difference of setA and setB
+       *
+       * @param {Set} setB - SetB
+       * @returns {Set} setC - difference between setA and setB
+       */
+
+    }, {
+      key: "difference",
+      value: function difference(setB) {
+        var setA = this;
+        var setC = new Set(setA);
+
+        var _iteratorNormalCompletion7 = true;
+        var _didIteratorError7 = false;
+        var _iteratorError7 = undefined;
+
+        try {
+          for (var _iterator7 = setB[Symbol.iterator](), _step7; !(_iteratorNormalCompletion7 = (_step7 = _iterator7.next()).done); _iteratorNormalCompletion7 = true) {
+            var v = _step7.value;
+
+            setC.delete(v);
+          }
+        } catch (err) {
+          _didIteratorError7 = true;
+          _iteratorError7 = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion7 && _iterator7.return) {
+              _iterator7.return();
+            }
+          } finally {
+            if (_didIteratorError7) {
+              throw _iteratorError7;
+            }
+          }
+        }
+
+        return setC;
+      }
+
+      /**
+       * @public
+       *
+       * @desc Symmetric difference of setA and setB
+       *
+       * @param {Set} setB - SetB
+       * @returns {Set} setC - difference difference between setA and setB
+       */
+
+    }, {
+      key: "symmetricDifference",
+      value: function symmetricDifference(setB) {
+        var setA = this;
+        var setC = new Set(setA);
+
+        var _iteratorNormalCompletion8 = true;
+        var _didIteratorError8 = false;
+        var _iteratorError8 = undefined;
+
+        try {
+          for (var _iterator8 = setB[Symbol.iterator](), _step8; !(_iteratorNormalCompletion8 = (_step8 = _iterator8.next()).done); _iteratorNormalCompletion8 = true) {
+            var v = _step8.value;
+
+            if (setA.has(v)) setC.delete(v);else setC.add(v);
+          }
+        } catch (err) {
+          _didIteratorError8 = true;
+          _iteratorError8 = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion8 && _iterator8.return) {
+              _iterator8.return();
+            }
+          } finally {
+            if (_didIteratorError8) {
+              throw _iteratorError8;
+            }
+          }
+        }
+
+        return setC;
+      }
+    }]);
 
     return _Set;
-  }(Set));
+  }(_extendableBuiltin(Set));
 
-  exports.default = _Set;
   exports.Set = _Set;
 
   Object.defineProperty(exports, '__esModule', { value: true });
