36b543a4100ab9a4d30b7339285d81f08522eb0e
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-14 14:04:02 -0700

    fix import for Array test

--- packages/Array/test/Array.spec.js
+++ packages/Array/test/Array.spec.js
@@ -1,5 +1,5 @@
 const { assert } = require("chai");
-const { Array } = require("../../../dist/super");
+const { Array } = require("../src/lib/Array");
 
 describe("Array", () => {
   let expected, actual;
