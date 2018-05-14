3d10cf4482ce25f629a846948f574d0cf5613fc7
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-14 09:05:01 -0700

    rebuild Set using rollup

--- packages/Set/lib/Set.js
+++ packages/Set/lib/Set.js
@@ -1,1 +1,222 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var _slicedToArray=function(){function a(a,b){var c=[],d=!0,e=!1,f=void 0;try{for(var g,h=a[Symbol.iterator]();!(d=(g=h.next()).done)&&(c.push(g.value),!(b&&c.length===b));d=!0);}catch(a){e=!0,f=a}finally{try{!d&&h["return"]&&h["return"]()}finally{if(e)throw f}}return c}return function(b,c){if(Array.isArray(b))return b;if(Symbol.iterator in Object(b))return a(b,c);throw new TypeError("Invalid attempt to destructure non-iterable instance")}}(),_createClass=function(){function a(a,b){for(var c,d=0;d<b.length;d++)c=b[d],c.enumerable=c.enumerable||!1,c.configurable=!0,"value"in c&&(c.writable=!0),Object.defineProperty(a,c.key,c)}return function(b,c,d){return c&&a(b.prototype,c),d&&a(b,d),b}}();function _classCallCheck(a,b){if(!(a instanceof b))throw new TypeError("Cannot call a class as a function")}function _possibleConstructorReturn(a,b){if(!a)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return b&&("object"==typeof b||"function"==typeof b)?b:a}function _inherits(a,b){if("function"!=typeof b&&null!==b)throw new TypeError("Super expression must either be null or a function, not "+typeof b);a.prototype=Object.create(b&&b.prototype,{constructor:{value:a,enumerable:!1,writable:!0,configurable:!0}}),b&&(Object.setPrototypeOf?Object.setPrototypeOf(a,b):a.__proto__=b)}function _extendableBuiltin(a){function b(){var b=Reflect.construct(a,Array.from(arguments));return Object.setPrototypeOf(b,Object.getPrototypeOf(this)),b}return b.prototype=Object.create(a.prototype,{constructor:{value:a,enumerable:!1,writable:!0,configurable:!0}}),Object.setPrototypeOf?Object.setPrototypeOf(b,a):b.__proto__=a,b}var _Set=function(a){function b(a){return _classCallCheck(this,b),_possibleConstructorReturn(this,(b.__proto__||Object.getPrototypeOf(b)).call(this,a))}return _inherits(b,a),_createClass(b,[{key:"some",value:function some(a){var b=void 0,c=!0,d=!1,e=void 0;try{for(var f,g=this.entries()[Symbol.iterator]();!(c=(f=g.next()).done);c=!0){var h=f.value,i=_slicedToArray(h,2),j=i[0],k=i[1];if(b=a(j,k,this),b)return!0}}catch(a){d=!0,e=a}finally{try{!c&&g.return&&g.return()}finally{if(d)throw e}}return!1}},{key:"every",value:function every(a){var b=void 0,c=!0,d=!1,e=void 0;try{for(var f,g=this.entries()[Symbol.iterator]();!(c=(f=g.next()).done);c=!0){var h=f.value,i=_slicedToArray(h,2),j=i[0],k=i[1];if(b=a(j,k,this),!b)return!1}}catch(a){d=!0,e=a}finally{try{!c&&g.return&&g.return()}finally{if(d)throw e}}return!0}},{key:"isSubset",value:function isSubset(a){var b=this,c=!0,d=!1,e=void 0;try{for(var f,g,h=b[Symbol.iterator]();!(c=(f=h.next()).done);c=!0)if(g=f.value,!a.has(g))return!1}catch(a){d=!0,e=a}finally{try{!c&&h.return&&h.return()}finally{if(d)throw e}}return!0}},{key:"isSuperset",value:function isSuperset(a){var b=this,c=!0,d=!1,e=void 0;try{for(var f,g,h=a[Symbol.iterator]();!(c=(f=h.next()).done);c=!0)if(g=f.value,!b.has(g))return!1}catch(a){d=!0,e=a}finally{try{!c&&h.return&&h.return()}finally{if(d)throw e}}return!0}},{key:"union",value:function union(a){var b=this,c=new Set(b),d=!0,e=!1,f=void 0;try{for(var g,h,i=a[Symbol.iterator]();!(d=(g=i.next()).done);d=!0)h=g.value,c.add(h)}catch(a){e=!0,f=a}finally{try{!d&&i.return&&i.return()}finally{if(e)throw f}}return c}},{key:"intersection",value:function intersection(a){var b=this,c=new Set,d=!0,e=!1,f=void 0;try{for(var g,h,i=a[Symbol.iterator]();!(d=(g=i.next()).done);d=!0)h=g.value,b.has(h)&&c.add(h)}catch(a){e=!0,f=a}finally{try{!d&&i.return&&i.return()}finally{if(e)throw f}}return c}},{key:"difference",value:function difference(a){var b=this,c=new Set(b),d=!0,e=!1,f=void 0;try{for(var g,h,i=a[Symbol.iterator]();!(d=(g=i.next()).done);d=!0)h=g.value,c.delete(h)}catch(a){e=!0,f=a}finally{try{!d&&i.return&&i.return()}finally{if(e)throw f}}return c}},{key:"symmetricDifference",value:function symmetricDifference(a){var b=this,c=new Set(b),d=!0,e=!1,f=void 0;try{for(var g,h,i=a[Symbol.iterator]();!(d=(g=i.next()).done);d=!0)h=g.value,b.has(h)?c.delete(h):c.add(h)}catch(a){e=!0,f=a}finally{try{!d&&i.return&&i.return()}finally{if(e)throw f}}return c}}]),b}(_extendableBuiltin(Set));exports.default=_Set,exports.Set=_Set;
+/**
+ * Copyright (c) 2018, Travis Clarke
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (factory((global.Set = {})));
+}(this, (function (exports) { 'use strict';
+
+  // 
+
+  /**
+   * @module super/set
+   *
+   */
+
+  /**
+   * @typedef {Function} Callback
+   */
+
+  /**
+   *
+   * Set with superpowers! 💪
+   *
+   * @public
+   *
+   */
+  var _Set = (function (Set) {
+    function _Set(iterable) {
+      Set.call(this, iterable);
+    }
+
+    if ( Set ) _Set.__proto__ = Set;
+    _Set.prototype = Object.create( Set && Set.prototype );
+    _Set.prototype.constructor = _Set;
+
+    /**
+     * @public
+     *
+     * @desc Tests whether at least one element in the set passes the test implemented by the provided function
+     *
+     * @param {Callback} callback - callback function
+     * @returns {boolean} true if the callback function returns a truthy value for any set element; otherwise, false
+     */
+    _Set.prototype.some = function some (callback) {
+      var this$1 = this;
+
+      var result;
+
+      for (var i = 0, list = this$1.entries(); i < list.length; i += 1) {
+        var ref = list[i];
+        var value1 = ref[0];
+        var value2 = ref[1];
+
+        result = callback(value1, value2, this$1);
+        if (result) { return true; }
+      }
+
+      return false;
+    };
+
+    /**
+     * @public
+     *
+     * @desc Test whether all elements in the set pass the test implemented by the provided function
+     *
+     * @param {Callback} callback - callback function
+     * @returns {boolean} true if the callback function returns a truthy value for every set element; otherwise, false
+     */
+    _Set.prototype.every = function every (callback) {
+      var this$1 = this;
+
+      var result;
+
+      for (var i = 0, list = this$1.entries(); i < list.length; i += 1) {
+        var ref = list[i];
+        var value1 = ref[0];
+        var value2 = ref[1];
+
+        result = callback(value1, value2, this$1);
+        if (!result) { return false; }
+      }
+
+      return true;
+    };
+
+    /**
+     * @public
+     *
+     * @desc Subset of a set
+     *
+     * @param {Set} setB - SetB
+     * @returns {boolean} setA is subset of setB
+     */
+    _Set.prototype.isSubset = function isSubset (setB) {
+      var setA = this;
+
+      for (var i = 0, list = setA; i < list.length; i += 1) {
+        var v = list[i];
+
+        if (!setB.has(v)) { return false; }
+      }
+      return true;
+    };
+
+    /**
+     * @public
+     *
+     * @desc Superset of a set
+     *
+     * @param {Set} setB - SetB
+     * @returns {boolean} setA is superset of setB
+     */
+    _Set.prototype.isSuperset = function isSuperset (setB) {
+      var setA = this;
+
+      for (var i = 0, list = setB; i < list.length; i += 1) {
+        var v = list[i];
+
+        if (!setA.has(v)) { return false; }
+      }
+      return true;
+    };
+
+    /**
+     * @public
+     *
+     * @desc Union of setA and setB
+     *
+     * @param {Set} setB - SetB
+     * @returns {Set} setC - union between setA and setB
+     */
+    _Set.prototype.union = function union (setB) {
+      var setA = this;
+      var setC = new Set(setA);
+
+      for (var i = 0, list = setB; i < list.length; i += 1) {
+        var v = list[i];
+
+        setC.add(v);
+      }
+
+      return setC;
+    };
+
+    /**
+     * @public
+     *
+     * @desc Intersection of setA and setB
+     *
+     * @param {Set} setB - SetB
+     * @returns {Set} setC - intersection between setA and setB
+     */
+    _Set.prototype.intersection = function intersection (setB) {
+      var setA = this;
+      var setC = new Set();
+
+      for (var i = 0, list = setB; i < list.length; i += 1) {
+        var v = list[i];
+
+        if (setA.has(v)) { setC.add(v); }
+      }
+
+      return setC;
+    };
+
+    /**
+     * @public
+     *
+     * @desc Difference of setA and setB
+     *
+     * @param {Set} setB - SetB
+     * @returns {Set} setC - difference between setA and setB
+     */
+    _Set.prototype.difference = function difference (setB) {
+      var setA = this;
+      var setC = new Set(setA);
+
+      for (var i = 0, list = setB; i < list.length; i += 1) {
+        var v = list[i];
+
+        setC.delete(v);
+      }
+
+      return setC;
+    };
+
+    /**
+     * @public
+     *
+     * @desc Symmetric difference of setA and setB
+     *
+     * @param {Set} setB - SetB
+     * @returns {Set} setC - difference difference between setA and setB
+     */
+    _Set.prototype.symmetricDifference = function symmetricDifference (setB) {
+      var setA = this;
+      var setC = new Set(setA);
+
+      for (var i = 0, list = setB; i < list.length; i += 1) {
+        var v = list[i];
+
+        if (setA.has(v)) { setC.delete(v); }
+        else { setC.add(v); }
+      }
+
+      return setC;
+    };
+
+    return _Set;
+  }(Set));
+
+  exports.default = _Set;
+  exports.Set = _Set;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
