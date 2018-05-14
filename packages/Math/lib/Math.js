d51ea07cec1673a9924f415157d6a47ec999ec3e
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-14 09:04:20 -0700

    rebuild Math using rollup

--- packages/Math/lib/Math.js
+++ packages/Math/lib/Math.js
@@ -1,1 +1,51 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var _Math=exports._Math=Object.create(Math);_Math.factorial=function(a){if(0>a)throw new Error("Factorial not defined for negative values");return 0===a?1:a*_Math.factorial(a-1)},exports.default=_Math,exports.Math=_Math;
+/**
+ * Copyright (c) 2018, Travis Clarke
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (factory((global.Math = {})));
+}(this, (function (exports) { 'use strict';
+
+  // 
+
+  /**
+   * @module super/math
+   *
+   */
+
+  /**
+   *
+   * Math with superpowers! 💪
+   *
+   * @public
+   *
+   * @alias Math
+   */
+  var _Math = Object.create(Math);
+
+  /**
+   * @public
+   *
+   * @desc Factorial
+   *
+   * @param {number} num - integral number
+   * @returns {number} factorial of num
+   */
+  _Math.factorial = function(num) {
+    if (num < 0) { throw new Error("Factorial not defined for negative values"); }
+    if (num === 0) { return 1; }
+    return num * _Math.factorial(num - 1);
+  };
+
+  exports._Math = _Math;
+  exports.default = _Math;
+  exports.Math = _Math;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
