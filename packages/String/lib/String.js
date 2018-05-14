bc6107d14e93998924c61ceb10a3bd60a96279a2
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-14 11:18:22 -0700

    prepare String

--- packages/String/lib/String.js
+++ packages/String/lib/String.js
@@ -11,11 +11,42 @@
   (factory((global.String = {})));
 }(this, (function (exports) { 'use strict';
 
-  // 
+  var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+  function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+  function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+  function _extendableBuiltin(cls) {
+    function ExtendableBuiltin() {
+      var instance = Reflect.construct(cls, Array.from(arguments));
+      Object.setPrototypeOf(instance, Object.getPrototypeOf(this));
+      return instance;
+    }
+
+    ExtendableBuiltin.prototype = Object.create(cls.prototype, {
+      constructor: {
+        value: cls,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+
+    if (Object.setPrototypeOf) {
+      Object.setPrototypeOf(ExtendableBuiltin, cls);
+    } else {
+      ExtendableBuiltin.__proto__ = cls;
+    }
+
+    return ExtendableBuiltin;
+  }
 
   /**
+   * 
    * @module super/string
-   *
    */
 
   /**
@@ -25,14 +56,21 @@
    * @public
    *
    */
-  var _String = (function (String) {
-    function _String(string) {
-      String.call(this, string);
-    }
+  var _String = function (_extendableBuiltin2) {
+    _inherits(_String, _extendableBuiltin2);
 
-    if ( String ) _String.__proto__ = String;
-    _String.prototype = Object.create( String && String.prototype );
-    _String.prototype.constructor = _String;
+    /**
+     * @public
+     *
+     * @desc Construct a String
+     *
+     * @param {string} string
+     */
+    function _String(string) {
+      _classCallCheck(this, _String);
+
+      return _possibleConstructorReturn(this, (_String.__proto__ || Object.getPrototypeOf(_String)).call(this, string));
+    }
 
     /**
      * @public
@@ -41,44 +79,52 @@
      *
      * @returns {string} String reversed
      */
-    _String.prototype.reverse = function reverse () {
-      return this.split("")
-        .filter(function (s) { return s; })
-        .reverse()
-        .join("");
-    };
 
-    /**
-     * @public
-     *
-     * @desc Transposes the ordering of the words in the string
-     *
-     * @returns {string} String with words reversed
-     */
-    _String.prototype.reverseWords = function reverseWords () {
-      return this.split(" ")
-        .filter(function (s) { return s; })
-        .reverse()
-        .join(" ");
-    };
 
-    /**
-     * @public
-     *
-     * @desc Convert a string to title case
-     *
-     * @returns {string} Title cased string representation
-     */
-    _String.prototype.toTitleCase = function toTitleCase () {
-      return this.split(" ")
-        .map(function (v) { return v && v.charAt(0).toUpperCase() + v.substr(1).toLowerCase(); })
-        .join(" ");
-    };
+    _createClass(_String, [{
+      key: "reverse",
+      value: function reverse() {
+        return this.split("").filter(function (s) {
+          return s;
+        }).reverse().join("");
+      }
+
+      /**
+       * @public
+       *
+       * @desc Transposes the ordering of the words in the string
+       *
+       * @returns {string} String with words reversed
+       */
+
+    }, {
+      key: "reverseWords",
+      value: function reverseWords() {
+        return this.split(" ").filter(function (s) {
+          return s;
+        }).reverse().join(" ");
+      }
+
+      /**
+       * @public
+       *
+       * @desc Convert a string to title case
+       *
+       * @returns {string} Title cased string representation
+       */
+
+    }, {
+      key: "toTitleCase",
+      value: function toTitleCase() {
+        return this.split(" ").map(function (v) {
+          return v && v.charAt(0).toUpperCase() + v.substr(1).toLowerCase();
+        }).join(" ");
+      }
+    }]);
 
     return _String;
-  }(String));
+  }(_extendableBuiltin(String));
 
-  exports.default = _String;
   exports.String = _String;
 
   Object.defineProperty(exports, '__esModule', { value: true });
--- packages/String/src/lib/String.js
+++ packages/String/src/lib/String.js
@@ -1,8 +1,6 @@
-// @flow
-
 /**
+ * @flow
  * @module super/string
- *
  */
 
 /**
