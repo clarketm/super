7e02689ed6b20a393f577b062e26a5a57a9b1452
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-14 09:05:26 -0700

    rebuild Queue using rollup

--- packages/Queue/lib/Queue.js
+++ packages/Queue/lib/Queue.js
@@ -1,1 +1,136 @@
-"use strict";var _createClass=function(){function a(a,b){for(var c,d=0;d<b.length;d++)c=b[d],c.enumerable=c.enumerable||!1,c.configurable=!0,"value"in c&&(c.writable=!0),Object.defineProperty(a,c.key,c)}return function(b,c,d){return c&&a(b.prototype,c),d&&a(b,d),b}}();Object.defineProperty(exports,"__esModule",{value:!0});function _toConsumableArray(a){if(Array.isArray(a)){for(var b=0,c=Array(a.length);b<a.length;b++)c[b]=a[b];return c}return Array.from(a)}function _classCallCheck(a,b){if(!(a instanceof b))throw new TypeError("Cannot call a class as a function")}var Queue=function(){function a(){var b=0<arguments.length&&void 0!==arguments[0]?arguments[0]:[];_classCallCheck(this,a),this._queue=[].concat(_toConsumableArray(b))}return _createClass(a,[{key:"isEmpty",value:function isEmpty(){return 0===this._queue.length}},{key:"clear",value:function clear(){this._queue.length=0}},{key:"enqueue",value:function enqueue(a){return this._queue.push(a)}},{key:"dequeue",value:function dequeue(){return this._queue.shift()}},{key:"front",value:function front(){return this._queue[0]}},{key:"rear",value:function rear(){return this._queue[this._queue.length-1]}},{key:"toArray",value:function toArray(){return this._queue.slice(0)}},{key:"size",get:function get(){return this._queue.length}}]),a}();exports.default=Queue,exports.Queue=Queue;
+/**
+ * Copyright (c) 2018, Travis Clarke
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (factory((global.Queue = {})));
+}(this, (function (exports) { 'use strict';
+
+  // 
+
+  /**
+   * @module super/queue
+   *
+   */
+
+  /**
+   * @typedef {null|undefined|boolean|number|string|Symbol|Function|Array|Date|Object} Item
+   */
+
+  /**
+   *
+   * Queue with superpowers! 💪
+   *
+   * @public
+   *
+   */
+  var Queue = function Queue(iterable) {
+    if ( iterable === void 0 ) iterable = [];
+
+    this._queue = [].concat( iterable );
+  };
+
+  var prototypeAccessors = { size: { configurable: true } };
+
+  /**
+   * @public
+   *
+   * @desc Get the current length of the queue
+   *
+   * @returns {number} length of the queue
+   */
+  prototypeAccessors.size.get = function () {
+    return this._queue.length;
+  };
+
+  /**
+   * @public
+   *
+   * @desc Check if queue is empty
+   *
+   * @returns {boolean} is queue empty
+   */
+  Queue.prototype.isEmpty = function isEmpty () {
+    return this._queue.length === 0;
+  };
+
+  /**
+   * @public
+   *
+   * @desc Clear the items from the queue
+   *
+   * @returns {void}
+   */
+  Queue.prototype.clear = function clear () {
+    this._queue.length = 0;
+  };
+
+  /**
+   * @public
+   *
+   * @desc Enqueue an item into the queue
+   *
+   * @param {Item} item - item to enqueue
+   * @returns {number} length after enqueue
+   */
+  Queue.prototype.enqueue = function enqueue (item) {
+    return this._queue.push(item);
+  };
+
+  /**
+   * @public
+   *
+   * @desc Dequeue an item from the queue
+   *
+   * @returns {Item} dequeued item
+   */
+  Queue.prototype.dequeue = function dequeue () {
+    return this._queue.shift();
+  };
+
+  /**
+   * @public
+   *
+   * @desc Get the front item in the queue
+   *
+   * @returns {Item} front item
+   */
+  Queue.prototype.front = function front () {
+    return this._queue[0];
+  };
+
+  /**
+   * @public
+   *
+   * @desc Get the rear item in the queue
+   *
+   * @returns {Item} rear item
+   */
+  Queue.prototype.rear = function rear () {
+    return this._queue[this._queue.length - 1];
+  };
+
+  /**
+   * @public
+   *
+   * @desc Convert the queue to an array
+   *
+   * @returns {Array} array representation of the queue
+   */
+  Queue.prototype.toArray = function toArray () {
+    return this._queue.slice(0);
+  };
+
+  Object.defineProperties( Queue.prototype, prototypeAccessors );
+
+  exports.default = Queue;
+  exports.Queue = Queue;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
