4557a9200b13016293ac4db7db50c06acc4dedb2
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-14 00:57:06 -0700

    add rollup config

---
+++ resources/rollup-config.js
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import fs from "fs";
+import path from "path";
+import { minify } from "uglify-js";
+import buble from "rollup-plugin-buble";
+import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
+import saveLicense from "uglify-save-license";
+import stripBanner from "rollup-plugin-strip-banner";
+import flow from "rollup-plugin-flow";
+
+const copyright = fs.readFileSync(path.join("resources", "COPYRIGHT"), "utf-8");
+
+const SRC_DIR = path.resolve("packages");
+const DIST_DIR = path.resolve("dist");
+
+export default {
+  input: path.join(SRC_DIR, "Super.js"),
+  output: {
+    name: "Super",
+    banner: copyright,
+    exports: "named",
+    file: path.join(DIST_DIR, "super.js"),
+    format: "umd"
+  },
+  plugins: [
+    flow({ pretty: true }),
+    commonjs(),
+    json(),
+    stripBanner(),
+    buble({ transforms: { dangerousForOf: true } }),
+    {
+      name: "uglify",
+      transformBundle(code) {
+        const result = minify(code, {
+          fromString: true,
+          mangle: { toplevel: true },
+          output: { max_line_len: 2048, comments: saveLicense },
+          compress: { comparisons: true, pure_getters: true, unsafe: true }
+        });
+
+        if (!fs.existsSync(DIST_DIR)) {
+          fs.mkdirSync(DIST_DIR);
+        }
+
+        fs.writeFileSync(
+          path.join(DIST_DIR, "super.min.js"),
+          result.code,
+          "utf8"
+        );
+      }
+    }
+  ]
+};
