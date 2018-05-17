2489a05d28e0adf13358ceb4277c5d7653fcf339
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-17 14:44:15 -0700

    rebuild dist

--- dist/super.es.js
+++ dist/super.es.js
@@ -1478,6 +1478,299 @@ var BinaryTree = function () {
 
 /**
  *
+ * Heap with superpowers! 💪
+ *
+ * @public
+ *
+ */
+
+var Heap = function () {
+
+  /** @private */
+
+  /** @private */
+
+  /**
+   * @public
+   *
+   * @desc Construct a Heap
+   *
+   * @param {Array<Item>} iterable
+   * @param {Comparator} comparator
+   */
+  function Heap() {
+    var iterable = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+    var comparator = arguments[1];
+    classCallCheck(this, Heap);
+
+    this._heap = [].concat(toConsumableArray(iterable));
+    this._compare = _compare(comparator || Heap._defaultComparator);
+
+    for (var i = Math.floor(iterable.length / 2); i >= 0; i--) {
+      this._percolateDown(i);
+    }
+  }
+
+  /**
+   * @private
+   *
+   * @desc Get the parent item in the heap (i.e. (i-1)/2)
+   *
+   * @param {number} index
+   * @returns {Item} _parent item
+   */
+
+
+  createClass(Heap, [{
+    key: "isEmpty",
+
+
+    /**
+     * @public
+     *
+     * @desc Check if heap is empty
+     *
+     * @returns {boolean} is heap empty
+     */
+    value: function isEmpty() {
+      return this._heap.length === 0;
+    }
+
+    /**
+     * @public
+     *
+     * @desc Clear the items from the heap
+     *
+     * @returns {void}
+     */
+
+  }, {
+    key: "clear",
+    value: function clear() {
+      this._heap.length = 0;
+    }
+
+    /**
+     * @public
+     *
+     * @desc Insert an item into the heap
+     *
+     * @param {Item} value - item to insert
+     * @returns {number} size after insert
+     */
+
+  }, {
+    key: "insert",
+    value: function insert(value) {
+      this._heap.push(value);
+      this._percolateUp(this.size - 1);
+      return this.size;
+    }
+
+    /**
+     * @public
+     *
+     * @desc Remove and return the maximum item
+     *
+     * @returns {Item} maximum item
+     */
+
+  }, {
+    key: "deleteMax",
+    value: function deleteMax() {
+      var _return = this._heap[0];
+      this._heap[0] = this._heap.pop();
+      this._percolateDown(0);
+      return _return;
+    }
+
+    /**
+     * @public
+     *
+     * @alias deleteMax
+     *
+     * @desc Remove and return the minimum item
+     *
+     * @returns {Item} minimum item
+     */
+
+  }, {
+    key: "deleteMin",
+    value: function deleteMin() {
+      return this.deleteMax();
+    }
+
+    /**
+     * @private
+     *
+     * @desc Heapify down
+     *
+     * @param {number} index - start index
+     */
+
+  }, {
+    key: "_percolateDown",
+    value: function _percolateDown(index) {
+      var left = Heap._left(index);
+      var right = Heap._right(index);
+      var root = index;
+
+      if (left < this.size && this._compare(this._heap[left], this._heap[index])) {
+        root = left;
+      }
+
+      if (right < this.size && this._compare(this._heap[right], this._heap[root])) {
+        root = right;
+      }
+
+      if (root !== index) {
+        swap(this._heap, index, root);
+        this._percolateDown(root);
+      }
+    }
+
+    /**
+     * @private
+     *
+     * @desc Heapify up
+     *
+     * @param {number} index - start index
+     */
+
+  }, {
+    key: "_percolateUp",
+    value: function _percolateUp(index) {
+      while (Heap._parent(index) >= 0) {
+        var parent = Heap._parent(index);
+        if (this._compare(this._heap[index], this._heap[parent])) {
+          swap(this._heap, index, parent);
+        }
+        index = parent;
+      }
+    }
+
+    /**
+     * @public
+     *
+     * @desc Convert the heap to an array
+     *
+     * @returns {Array<Item>} array representation of the heap
+     */
+
+  }, {
+    key: "toArray",
+    value: function toArray$$1() {
+      return this._heap.slice(0);
+    }
+  }, {
+    key: "size",
+
+
+    /**
+     * @public
+     *
+     * @desc Get the current size of the heap
+     *
+     * @returns {number} size of the heap
+     */
+    get: function get$$1() {
+      return this._heap.length;
+    }
+
+    /**
+     * @public
+     *
+     * @desc Get the maximum item in heap
+     *
+     * @returns {Item} maximum item
+     */
+
+  }, {
+    key: "max",
+    get: function get$$1() {
+      return this._heap[0];
+    }
+
+    /**
+     * @public
+     *
+     * @alias max
+     *
+     * @desc Get the minimum item in heap
+     *
+     * @returns {Item} minimum item
+     */
+
+  }, {
+    key: "min",
+    get: function get$$1() {
+      return this.max;
+    }
+  }], [{
+    key: "_parent",
+    value: function _parent(index) {
+      return Math.floor((index - 1) / 2);
+    }
+
+    /**
+     * @private
+     *
+     * @desc Get the left child (i.e. 2*i+1)
+     *
+     * @param {number} index
+     * @returns {Item} _left child
+     */
+
+  }, {
+    key: "_left",
+    value: function _left(index) {
+      return 2 * index + 1;
+    }
+
+    /**
+     * @private
+     *
+     * @desc Get the right child (i.e. 2*i+2)
+     *
+     * @param {number} index
+     * @returns {Item} _left child
+     */
+
+  }, {
+    key: "_right",
+    value: function _right(index) {
+      return 2 * index + 2;
+    }
+
+    /**
+     * @private
+     *
+     * @desc Default comparator function to sort from:
+     *       highest priority (max) -> lowest priority (min)
+     *
+     * @returns {number} size of the queue
+     */
+
+  }, {
+    key: "_defaultComparator",
+    value: function _defaultComparator$$1(a, b) {
+      return a - b;
+    }
+  }]);
+  return Heap;
+}();
+
+Heap.HeapType = {
+  MIN: function MIN(a, b) {
+    return a - b;
+  },
+  MAX: function MAX(a, b) {
+    return b - a;
+  }
+};
+
+/**
+ *
  * ListNode
  *
  * @public
@@ -2569,25 +2862,24 @@ var QueueNode = function () {
   return QueueNode;
 }();
 
-var commonjsGlobal = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
-
-/** Detect free variable `global` from Node.js. */
-var freeGlobal = _typeof(commonjsGlobal) == 'object' && commonjsGlobal && commonjsGlobal.Object === Object && commonjsGlobal;
-
-var _freeGlobal = freeGlobal;
+var freeGlobal = require('./_freeGlobal');
 
 /** Detect free variable `self`. */
 var freeSelf = (typeof self === 'undefined' ? 'undefined' : _typeof(self)) == 'object' && self && self.Object === Object && self;
 
 /** Used as a reference to the global object. */
-var root = _freeGlobal || freeSelf || Function('return this')();
+var root = freeGlobal || freeSelf || Function('return this')();
 
-var _root = root;
+module.exports = root;
+
+var _root = /*#__PURE__*/Object.freeze({
+
+});
 
 /** Built-in value references. */
-var _Symbol2 = _root.Symbol;
+var _Symbol = _root.Symbol;
 
-var _Symbol = _Symbol2;
+var _Symbol_1 = _Symbol;
 
 /** Used for built-in method references. */
 var objectProto = Object.prototype;
@@ -2603,7 +2895,7 @@ var hasOwnProperty = objectProto.hasOwnProperty;
 var nativeObjectToString = objectProto.toString;
 
 /** Built-in value references. */
-var symToStringTag = _Symbol ? _Symbol.toStringTag : undefined;
+var symToStringTag = _Symbol_1 ? _Symbol_1.toStringTag : undefined;
 
 /**
  * A specialized version of `baseGetTag` which ignores `Symbol.toStringTag` values.
@@ -2661,7 +2953,7 @@ var nullTag = '[object Null]',
     undefinedTag = '[object Undefined]';
 
 /** Built-in value references. */
-var symToStringTag$1 = _Symbol ? _Symbol.toStringTag : undefined;
+var symToStringTag$1 = _Symbol_1 ? _Symbol_1.toStringTag : undefined;
 
 /**
  * The base implementation of `getTag` without fallbacks for buggy environments.
@@ -2671,10 +2963,10 @@ var symToStringTag$1 = _Symbol ? _Symbol.toStringTag : undefined;
  * @returns {string} Returns the `toStringTag`.
  */
 function baseGetTag(value) {
-  if (value == null) {
-    return value === undefined ? undefinedTag : nullTag;
-  }
-  return symToStringTag$1 && symToStringTag$1 in Object(value) ? _getRawTag(value) : _objectToString(value);
+    if (value == null) {
+        return value === undefined ? undefinedTag : nullTag;
+    }
+    return symToStringTag$1 && symToStringTag$1 in Object(value) ? _getRawTag(value) : _objectToString(value);
 }
 
 var _baseGetTag = baseGetTag;
@@ -2696,7 +2988,11 @@ function isObjectLike(value) {
   return value != null && (typeof value === 'undefined' ? 'undefined' : _typeof(value)) == 'object';
 }
 
-var isObjectLike_1 = isObjectLike;
+module.exports = isObjectLike;
+
+var isObjectLike$1 = /*#__PURE__*/Object.freeze({
+
+});
 
 /** `Object#toString` result references. */
 var objectTag = '[object Object]';
@@ -2743,7 +3039,7 @@ var objectCtorString = funcToString.call(Object);
  * // => true
  */
 function isPlainObject(value) {
-  if (!isObjectLike_1(value) || _baseGetTag(value) != objectTag) {
+  if (!isObjectLike$1(value) || _baseGetTag(value) != objectTag) {
     return false;
   }
   var proto = _getPrototype(value);
@@ -3979,13 +4275,14 @@ var Trie = function () {
   return Trie;
 }();
 
-var version = "0.0.6";
+var version = "0.0.7";
 
 var Super = {
   version: version,
   // Data Structures
   Array: _Array,
   BinaryTree: BinaryTree,
+  Heap: Heap,
   LinkedList: LinkedList,
   Map: _Map,
   Object: _Object,
@@ -4006,4 +4303,4 @@ var Super = {
 };
 
 export default Super;
-export { version, _Array as Array, BinaryTree, LinkedList, _Map as Map, _Object as Object, PriorityQueue, Queue, _Set as Set, Trie, _Math as Math, _Number as Number, _String as String, bubbleSort, insertionSort, mergeSort, quickSort, selectionSort };
+export { version, _Array as Array, BinaryTree, Heap, LinkedList, _Map as Map, _Object as Object, PriorityQueue, Queue, _Set as Set, Trie, _Math as Math, _Number as Number, _String as String, bubbleSort, insertionSort, mergeSort, quickSort, selectionSort };
