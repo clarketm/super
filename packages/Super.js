e9dfd758f569db0d849f1e0f0a8fd72584e42a85
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-17 08:51:24 -0700

    add insertionSort and selectionSort to build

--- packages/Super.js
+++ packages/Super.js
@@ -8,6 +8,7 @@
 import { Array } from "./Array/src/lib/Array";
 import { BinaryTree } from "./BinaryTree/src/lib/BinaryTree";
 import { bubbleSort } from "./BubbleSort/src/lib/BubbleSort";
+import { insertionSort } from "./InsertionSort/src/lib/InsertionSort";
 import { LinkedList } from "./LinkedList/src/lib/LinkedList";
 import { Map } from "./Map/src/lib/Map";
 import { Math } from "./Math/src/lib/Math";
@@ -17,6 +18,7 @@ import { Object } from "./Object/src/lib/Object";
 import { PriorityQueue } from "./PriorityQueue/src/lib/PriorityQueue";
 import { Queue } from "./Queue/src/lib/Queue";
 import { quickSort } from "./QuickSort/src/lib/QuickSort";
+import { selectionSort } from "./SelectionSort/src/lib/SelectionSort";
 import { Set } from "./Set/src/lib/Set";
 import { String } from "./String/src/lib/String";
 import { Trie } from "./Trie/src/lib/Trie";
@@ -40,8 +42,10 @@ export default {
   String,
   // Sorting Algorithms
   bubbleSort,
+  insertionSort,
   mergeSort,
-  quickSort
+  quickSort,
+  selectionSort
 };
 
 export {
@@ -62,6 +66,8 @@ export {
   String,
   // Sorting Algorithms
   bubbleSort,
+  insertionSort,
   mergeSort,
-  quickSort
+  quickSort,
+  selectionSort
 };
